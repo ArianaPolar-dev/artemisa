110b5c6aec4322c1dda2231eb072fcb10f8b97db
ArianaPolar-dev <polar3dev@gmail.com>
2024-11-15 23:38:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const fallingText = document.querySelector('.falling-text');
 
 // Generar la forma de un corazón usando palabras "Te amo"
 const heartPattern = [
-    "       Te amo amo       ",
-    "    Te amo Te amo Te amo    ",
-    "  Te amo Te amo Te amo Te   ",
-    "Te amo Te amo Te amo Te amo ",
-    " Te amo Te amo Te amo Te am",
-    "   Te amo Te amo Te amo Te a  ",
-    "     Te amo Te amo Te amo    ",
-    "        Te amo Te amo       ",
-    "          Te amo            "
+    "         Te amo         ",
+    "      Te amo Te amo      ",
+    "   Te amo Te amo Te amo   ",
+    " Te amo Te amo Te amo Te amo ",
+    "Te amo Te amo Te amo Te amo Te amo",
+    " Te amo Te amo Te amo Te amo Te amo ",
+    "   Te amo Te amo Te amo Te amo   ",
+    "     Te amo Te amo Te amo     ",
+    "       Te amo Te amo       ",
+    "         Te amo         ",
+    "          Te           "
 ];
 
 // Crear un flujo continuo de texto
